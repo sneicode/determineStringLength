42adf8fe88a2718c3e148341d9774616e48e531d
sneicode <68944040+sneicode@users.noreply.github.com>
2021-05-25 11:21:34 +0200

    updated solidity version for easier string handling, added nameValidator function

--- utf8StringLength.sol
+++ utf8StringLength.sol
@@ -1,29 +1,32 @@
 pragma solidity 0.8.4;
 
-contract utf8StringLength {
+contract utf8String {
     
     //---------------------BEGIN Code to copy-paste--------------
 
-    function utfStringLength(string memory str) public returns (uint) {
-        uint length = 0;
-        uint i=0;
-        string memory string_rep = bytes(str);
 
-        while (i<string_rep.length)
-        {
-            if (string_rep[i]>>7==0)
-                i+=1;
-            else if (string_rep[i]>>5==0x6)
-                i+=2;
-            else if (string_rep[i]>>4==0xE)
-                i+=3;
-            else if (string_rep[i]>>3==0x1E)
-                i+=4;
-            else
-                //For safety
-                i+=1;
-
-            length++;
-        }
+    function nameValidator(string memory str) public returns (bool){
+        
+        uint length = _utf8StringLength(str);
+        
+        require(length <= 20, "Name too long, length is limited to 20 characters");
+        require(length > 3, "Name too short, please assure a minimum of 4 characters");
+        
+        return true;
+        
+        // how to commit the name to the cat struct
     }
+
+    function _utf8StringLength(string memory str) private pure returns (uint) {
+        
+        return bytes(str).length;
+    }
+    
+    // this code allows for spaces between characters, should the code prevent this? (max the killer cat)
+    // CHANGE / UPDATE CAT NAMES? opinion: changing cat names kind of self-defeats the uniqueness of an NFT
+    // Question: once assigned, should the user be allowed to change the name(s) of her cat(s)? 
+    // --> Maybe once and against a (high) fee 
+    // --> maybe allow a single change per cat with a time limit, i.e. 1 week after creation name change no longer possible.
+    
+    
 }
